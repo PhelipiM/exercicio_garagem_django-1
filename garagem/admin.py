5e27b25aa0619c85c0c804fbac9b0bd3d3333e99
Nunes-Will <nuneswillroberto@gmail.com>
2023-03-10 16:48:46 -0300

    Start_projeto

---
+++ garagem/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Marcas
+
+admin.site.register(Marcas)
+
+# Register your models here.
---
+++ garagem/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Marcas(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+# Create your models here.
