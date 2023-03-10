e4b277399f2347e06c9d5325e6b45ef8fffd9375
Nunes-Will <nuneswillroberto@gmail.com>
2023-03-10 16:54:11 -0300

    Caixa_aulta

--- garagem/models.py
+++ garagem/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Marcas(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, null=True, blank=True)
+    descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.nome
+        return self.nome.upper()
 
 # Create your models here.
