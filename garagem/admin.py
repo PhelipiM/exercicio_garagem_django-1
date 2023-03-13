1243720c635e764c5223b0beb8996437272fae56
Nunes-Will <nuneswillroberto@gmail.com>
2023-03-13 09:26:16 -0300

    import categoria

--- garagem/admin.py
+++ garagem/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Marcas
 
+from .models import Categoria
+
 admin.site.register(Marcas)
+
+admin.site.register(Categoria)
 
 # Register your models here.
--- garagem/models.py
+++ garagem/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+
 class Marcas(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, null=True, blank=True)
-    descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome.upper()
+
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
 
 # Create your models here.
