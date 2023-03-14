0649287bdc0f416215031a40c8fbaba1c791edfd
Nunes-Will <nuneswillroberto@gmail.com>
2023-03-14 15:04:34 -0300

    Makemigrate

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 
-from .models import Marca
-
-from .models import Categoria
-
-admin.site.register(Marca)
+from .models import Categoria, Marca
 
 admin.site.register(Categoria)
-
-# Register your models here.
+admin.site.register(Marca)
