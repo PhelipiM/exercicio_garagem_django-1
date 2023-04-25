d6dee6957f3afac48a2095813c9cd1004dfde8f0
Nunes-Will <nuneswillroberto@gmail.com>
2023-04-25 14:14:09 -0300

    Add AcessórioSerializer

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from garagem.views import MarcaViewSet, CategoriaViewSet, CorViewSet
+from garagem.views import MarcaViewSet, CategoriaViewSet, CorViewSet, AcessorioViewSet
 
 router = DefaultRouter()
 router.register(r"marcas", MarcaViewSet)
 router.register(r"categoria", CategoriaViewSet)
 router.register(r"cor", CorViewSet)
+router.register(r"acessório", AcessorioViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
