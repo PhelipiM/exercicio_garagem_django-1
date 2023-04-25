fb4525bfb01d7bd4ac7434eb63cdc0418deeb268
Nunes-Will <nuneswillroberto@gmail.com>
2023-04-25 14:01:33 -0300

    Add CorSerializer

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,12 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from garagem.views import MarcaViewSet, CategoriaViewSet
+from garagem.views import MarcaViewSet, CategoriaViewSet, CorViewSet
 
 router = DefaultRouter()
 router.register(r"marcas", MarcaViewSet)
 router.register(r"categoria", CategoriaViewSet)
+router.register(r"cor", CorViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
