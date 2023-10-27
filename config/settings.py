4029e25be046dd751ae45aa57d730dec46ef98b8
PhelipiM <ggizmeumigo@gmail.com>
2023-10-27 14:49:08 -0300

    finalização do passo 21

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,22 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = "usuario.Usuario"
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MODE = os.getenv("MODE")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "False")
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://localhost:8000", "https://*.fl0.io/"]
+...
+
+if MODE in ["PRODUCTION", "MIGRATE"]:
+    MEDIA_URL = '/media/' 
+else:    
+    MY_IP = os.getenv("MY_IP", "127.0.0.1")
+    MEDIA_URL = f"http://{MY_IP}:19003/media/"
